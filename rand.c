f1901314d320247be364a87f7e36d8698e358876
Andrew Bainbridge <andbain@microsoft.com>
2017-08-25 17:40:44 +0100

    The PCG random number generator doesn't seem to work in Visual Studio, so I added Xoroshiro128+ as an alternative.

--- rand.c
+++ rand.c
@@ -21,3 +21,22 @@ uint32_t fastrand2()
     uint32_t rot = oldstate >> 59u;
     return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
 }
+
+
+// Xoroshiro128+
+static inline uint64_t rotl(const uint64_t x, int k) {
+	return (x << k) | (x >> (64 - k));
+}
+
+uint64_t rand64() {
+	static uint64_t s[2] = { 0x853c49e6748fea9bull, 0xda3e39cb94b95bdbull };
+	const uint64_t s0 = s[0];
+	uint64_t s1 = s[1];
+	const uint64_t result = s0 + s1;
+
+	s1 ^= s0;
+	s[0] = rotl(s0, 55) ^ s1 ^ (s1 << 14); // a, b
+	s[1] = rotl(s1, 36); // c
+
+	return result;
+}
